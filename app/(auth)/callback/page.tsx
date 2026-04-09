1bd347a3e386dfa6ffd51e8257509c85955ae51c
HadegoldDevelopers <developers@hadegoldmedia.com>
2026-04-09 18:25:03 +0100

    Email Verification

--- app/(auth)/callback/page.tsx
+++ app/(auth)/callback/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { useEffect } from "react";
-import { supabaseBrowser } from "@/lib/supabase/supabaseBrowser";
+import { supabase } from "@/lib/supabase/supabaseClient";
 
 export default function AuthCallback() {
   useEffect(() => {
-    supabaseBrowser().auth.exchangeCodeForSession(window.location.href);
+    supabase.auth.exchangeCodeForSession(window.location.href);
   }, []);
 
   return <p>Verifying your email…</p>;
